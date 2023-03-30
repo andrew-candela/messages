27a5f64a1401d954f1fdfef389e99eec06dd80a7
acandela260 <20376875+andrew-candela@users.noreply.github.com>
2023-03-30 10:05:11 -0700

    adds another rsa test

--- messages/rsa_test.go
+++ messages/rsa_test.go
@@ -1,6 +1,7 @@
 package messages
 
 import (
+	"os"
 	"testing"
 )
 
@@ -11,5 +12,19 @@ func TestCryptoEncode(t *testing.T) {
 	decoded := RSADecrypt(*k, cipher)
 	if string(decoded) != message {
 		t.Errorf("%s != %s", message, string(decoded))
+	}
+}
+
+func TestRSAWriteRead(t *testing.T) {
+	message := []byte("hello world")
+	test_key_file := "test_key.pem"
+	k := GenerateRandomKey()
+	WriteKeyToDisk(k, test_key_file)
+	defer os.Remove(test_key_file)
+	new_key, _ := ReadExistingKey(test_key_file)
+	cipher, _ := RSAEncrypt(new_key.PublicKey, message)
+	decoded := RSADecrypt(*new_key, cipher)
+	if string(decoded) != string(message) {
+		t.Errorf("%s != %s", message, decoded)
 	}
 }
